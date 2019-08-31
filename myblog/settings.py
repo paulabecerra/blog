e8c83daeef561136db024009e9c921f261f62d64
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-08-31 08:06:50 -0500

    Debug y autenticación para agregar y editar

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(95$%p+@q8vo*a&d=-a=1_@lacy-z+!$9_9q@qfyc8ssa+gi#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
