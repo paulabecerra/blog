9dae1e0dbc27a121a7a9362d787f9c4b9e3b4b1b
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-08-31 01:37:44 -0500

    Se terminaron las vistas base

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,15 +1,46 @@
 #Django
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 #Blogpost
 from .models import Post
+from .forms import PostForm
 
 #Post List#
 def post_list(request):
-    posts = Post.objects.all().order_by('published_date')
+    posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blogpost/post_list.html', {'posts':posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blogpost/post_detail.html', {'post':post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blogpost/post_new.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blogpost/post_new.html', {'form': form})
