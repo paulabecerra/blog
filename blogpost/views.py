e2e21b97a52e60f071654852034298f0580623bb
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-08-31 00:20:29 -0500

    Estamos extendiendo las funcionalidades de la aplicación

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,5 +1,5 @@
 #Django
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 #Blogpost
@@ -9,3 +9,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'blogpost/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogpost/post_detail.html', {'post':post})
