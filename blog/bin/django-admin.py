4a492b90878128d19670a66f6e346313113ebe4c
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-08-30 19:40:01 -0500

    Mi aplicación Django Girls, primer commit

---
+++ blog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/paulabecerra/Desktop/Archivos_Python/Proyectos/paulablog/blog/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
