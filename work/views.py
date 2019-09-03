8665223b5de27e0ae40f7e4f2abbdcac6a353fe8
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-09-03 16:13:02 -0500

    I fixed a couple of bugs and the work experience is now displayed

--- work/views.py
+++ work/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Work
 
 def work_list(request):
-    works = Work.objects.all().order_by('-start_date')
+    works = Work.objects.all()
     return render(request, 'work/work_positions.html', {'works':works})
 
 # Create your views here.
