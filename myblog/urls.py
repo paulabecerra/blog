fd30f1eab69f7708ad9b5fa54f3753c781e961ba
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-09-03 14:42:51 -0500

    I checked the urls from home

--- myblog/urls.py
+++ myblog/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('blogpost.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
