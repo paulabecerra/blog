c95b7c5a2d2947b7eb77f73c62309c6a00ac0188
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-09-03 16:31:44 -0500

    I created the work detail

--- work/urls.py
+++ work/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.work_list, name='work_list'),
+    path('<int:pk>/', views.work_detail, name='work_detail'),
+
 ]
--- work/views.py
+++ work/views.py
@@ -7,4 +7,9 @@ def work_list(request):
     works = Work.objects.all()
     return render(request, 'work/work_positions.html', {'works':works})
 
+
+def work_detail(request):
+    work = get_object_or_404(Work, pk=pk)
+    return render(request, 'work/work_detail.html')
+
 # Create your views here.
